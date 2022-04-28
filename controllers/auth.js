0ea99672eaf5a5eb3e87c217c6224d3a36264024
yuvraj-2503 <singh.yuvraj1047@gmail.com>
2022-04-28 22:29:45 +0530

    CASE SENSITIVE QUERY ON SIGNUP

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,6 +18,14 @@ exports.signup = async (req, res) =>{
     }
     
     const { name, email, password , college, department } = req.body;
+    let u1 = User.findOne({ email : {email , $caseSensitive : false } }, );
+    if(u1){
+        return res.status(400).json({
+            "statusCode" : 400,
+            "developerMessage": "email already exists..login to continue",
+            "result" : null
+        });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
         name : name,
